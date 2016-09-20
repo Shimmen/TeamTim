326a807f4b03983e5c835910f24e495c653f9a53
Marcus <marsjos@student.chalmers.se>
2016-09-20 11:48:09 +0200

    Create textfield and check input against answer

--- android/app/src/main/java/teamtim/teamtimapp/PlayActivity.java
+++ android/app/src/main/java/teamtim/teamtimapp/PlayActivity.java
@@ -2,11 +2,16 @@ package teamtim.teamtimapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class PlayActivity extends AppCompatActivity {
 
     private ImageView imageView;
+    private EditText answerText;
+    private String check;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +19,17 @@ public class PlayActivity extends AppCompatActivity {
         setContentView(R.layout.activity_play);
         imageView = (ImageView) findViewById(R.id.imageView);
         imageView.setImageResource(R.mipmap.ic_launcher);
+    }
+
+    public void checkAnswer(View v){
+        answerText = (EditText) findViewById(R.id.answerTextField);
+        check = answerText.getText().toString();
+        if (check.equals("Android")) {
+            Log.d("Answer:", "Correct");
+        }
+        else{
+            Log.d("Answer:", "Wrong");
+        }
+
     }
 }
