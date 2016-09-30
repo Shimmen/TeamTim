b7ee02e7e651e49c7b6e2c658521e325fb403890
Marcus <marsjos@student.chalmers.se>
2016-09-30 12:25:57 +0200

    List peers in view and create buttons for each

--- android/app/src/main/java/teamtim/teamtimapp/activities/MultiplayerActivity.java
+++ android/app/src/main/java/teamtim/teamtimapp/activities/MultiplayerActivity.java
@@ -1,20 +1,27 @@
 package teamtim.teamtimapp.activities;
 
+import android.content.DialogInterface;
 import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import teamtim.teamtimapp.R;
 import teamtim.teamtimapp.network.DefaultNetworkManager;
 
-public class MultiplayerActivity extends AppCompatActivity {
+public class MultiplayerActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private LinearLayout userList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_friends);
+        userList = (LinearLayout) findViewById(R.id.user_list);
 
         System.out.println("Begin looking for peers!");
         DefaultNetworkManager.getDefault().beginDiscoveringPeers(new WifiP2pManager.PeerListListener() {
@@ -22,7 +29,7 @@ public class MultiplayerActivity extends AppCompatActivity {
             public void onPeersAvailable(WifiP2pDeviceList peers) {
                 System.out.println("Available devices:");
                 for (WifiP2pDevice device : peers.getDeviceList()) {
-                    System.out.println(device);
+                    createButtons(device.toString());
                 }
             }
         });
@@ -32,5 +39,17 @@ public class MultiplayerActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         DefaultNetworkManager.getDefault().stopDiscoveringPeers();
+    }
+
+    private void createButtons(String text){
+        Button butt = new Button(getApplicationContext());
+        butt.setText(text);
+        butt.setOnClickListener(this);
+        userList.addView(butt);
+    }
+
+    @Override
+    public void onClick(View v) {
+        System.out.print("You clicked me");
     }
 }
