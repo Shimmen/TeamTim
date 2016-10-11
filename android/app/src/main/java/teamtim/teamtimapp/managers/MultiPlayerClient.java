11414d8f042bfdec61523568cdbb256eb72e8493
Simon Moos <simon.moos95@gmail.com>
2016-10-11 11:01:53 +0200

    Push the next question to the UI

--- android/app/src/main/java/teamtim/teamtimapp/managers/MultiPlayerClient.java
+++ android/app/src/main/java/teamtim/teamtimapp/managers/MultiPlayerClient.java
@@ -4,7 +4,6 @@ import java.net.InetAddress;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 import teamtim.teamtimapp.NetworkUtil;
 import teamtim.teamtimapp.activities.PlayActivity;
@@ -42,36 +41,26 @@ public class MultiPlayerClient extends QuestionResultListener implements ClientT
     public void onData(Map<String, String> data) {
         String clientName = clientThread.getName();
 
-        try {
-            switch (data.get("METHOD")) {
+        switch (data.get("METHOD")) {
 
 
-                case "NEW_QUESTION":
-                    WordQuestion currentQuestion = NetworkUtil.decodeQuestion(data.get("QUESTION"));
-                    System.out.println(clientName + ": received new question: " + data + ", i.e., " + currentQuestion.getWord());
+            case "NEW_QUESTION":
+                WordQuestion currentQuestion = NetworkUtil.decodeQuestion(data.get("QUESTION"));
+                System.out.println(clientName + ": received new question: " + data + ", i.e., " + currentQuestion.getWord());
 
-                    System.out.println(clientName + ": answering question...");
-                    Thread.sleep(1000);
-                    int currentQuestionResult = 10 + new Random().nextInt(10);
+                // Load next question
+                currentPlayActivity.newQuestion(currentQuestion);
+                break;
 
-                    System.out.println(clientName + ": sending question results (" + currentQuestionResult + ")!");
-                    Map<String, String> resultData = new HashMap<>();
-                    resultData.put("QUESTION_RESULT", String.valueOf(currentQuestionResult));
-                    clientThread.addDataToSendQueue(resultData);
-                    break;
+            case "GAME_RESULTS":
+                System.out.println(clientName + ": received game results: " + data);
+                // TODO: Use data!
+                updateScore(0, 0);
+                break;
 
-                case "GAME_RESULTS":
-                    System.out.println(clientName + ": received game results: " + data);
-                    // TODO: Use data!
-                    updateScore(0, 0);
-                    break;
+            default:
+                System.err.println(clientName + ": got some unknown packet?!" + data);
 
-                default:
-                    System.err.println(clientName + ": got some unknown packet?!" + data);
-
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
     }
 
@@ -84,13 +73,14 @@ public class MultiPlayerClient extends QuestionResultListener implements ClientT
     public void onQuestionResult(int result) {
         System.out.println("MultiPlayerClient: got some question results!");
 
+        System.out.println(clientThread.getName() + ": sending question results (" + result + ")!");
         Map<String, String> resultData = new HashMap<>();
         resultData.put("QUESTION_RESULT", String.valueOf(result));
         clientThread.addDataToSendQueue(resultData);
     }
 
     public void updateScore(int player1, int player2){
-        //Something
+        // TODO: Implement!
     }
 
 }
