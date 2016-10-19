f13f6b941b1c0df0245f7659abfe6b840d320d77
Simon Moos <simon.moos95@gmail.com>
2016-10-19 10:56:23 +0200

    Add demo gif to main menu

--- android/app/src/main/java/teamtim/teamtimapp/Activities/MainMenuActivity.java
+++ android/app/src/main/java/teamtim/teamtimapp/Activities/MainMenuActivity.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.webkit.DownloadListener;
+import android.webkit.WebView;
 
 import teamtim.teamtimapp.R;
 
@@ -14,8 +18,17 @@ public class MainMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-        //Intent intentMain = new Intent(MainMenuActivity.this, DemoActivity.class);
-        //MainMenuActivity.this.startActivity(intentMain);
+        WebView demoWebView = (WebView) findViewById(R.id.demoWebView);
+        //demoWebView.setAlpha(0.0f);
+
+        // Load the contents...
+        demoWebView.getSettings().setLoadWithOverviewMode(true);
+        demoWebView.getSettings().setUseWideViewPort(true);
+        demoWebView.loadUrl("file:///android_res/raw/demo_stava.gif");
+
+        // ...then fade in
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        demoWebView.startAnimation(fadeIn);
     }
 
     public void playGame(View v){
